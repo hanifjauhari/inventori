899a59079e45da90a3dc2da4ead0f956419479fa
iqbalmirza13 <60764455+iqbalmirza13@users.noreply.github.com>
2021-05-31 23:39:45 +0700

    halaman utama data pegawai

---
+++ application/views/pegawai/datapegawai.php
@@ -0,0 +1,95 @@
+<!-- Karena sudah ada template header footer, langsung copas kontennya -->
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Data pegawai</h1>
+                    <p>data dari pegawai CV Dwi Tunggal Abadi</p>
+
+
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Layout</a></li>
+                        <li class="breadcrumb-item active">Fixed Navbar Layout</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col">
+                    <!-- Default box -->
+
+                    <?php echo $this->session->flashdata('pesan') ?>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Table data pegawai</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <table id="active-datatable" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>nama pegawai</th>
+                                        <th>gender</th>
+                                        <th>telp</th>
+                                        <th>email</th>
+                                        <th>status</th>
+                                        <th>foto</th>
+                                        <th>opsi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($data_pegawai->result_array() as $kolom) :   ?>
+                                        <tr>
+                                            <td width="5%"><?php echo $kolom['nama_pegawai']   ?></td>
+                                            <td width="2%"><?php echo $kolom['gender'] ?></td>
+                                            <td width="2%"><?php echo $kolom['telp'] ?></td>
+                                            <td width="2%"><?php echo $kolom['email'] ?></td>
+                                            <td><?php echo $kolom['status'] ?></td>
+                                            <td width="5%">
+
+                                                <?php
+
+                                                $namaFoto = $kolom['foto'];
+                                                $alamatFoto = base_url('assets/dist/img/pegawai/' . $namaFoto);
+                                                ?>
+
+                                                <img src="<?php echo $alamatFoto ?>" alt="<?php echo $namaFoto ?>" style="width: 100px">
+
+                                            </td>
+                                            <td>
+                                                </a>
+                                            </td>
+
+                                        </tr>
+                                    <?php endforeach; ?>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
